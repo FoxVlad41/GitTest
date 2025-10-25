3a9a89c17ca28ee28d6c63bcd9f0a20e22ce5083
FoxVlad41 <@email.com>
2025-10-25 19:48:31 +0500

    СТ ПР6

--- todos/todo.py
+++ todos/todo.py
@@ -16,7 +16,7 @@ async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
         "todos": todo_list
     })
 
-@todo_router.get("/todo", response_model=TodoItems)
+@todo_router.get("/todo")
 async def retrieve_todos(request: Request):
     return templates.TemplateResponse("todo.html", {
         "request": request,
@@ -34,7 +34,7 @@ async def get_single_todo(request: Request, todo_id: int = Path(..., title="The 
             })
     raise HTTPException( status_code=status.HTTP_404_NOT_FOUND,detail="Todo with supplied ID doesn't exist",)
 
-@todo_router.put("/todo/{todo_ids}")
+@todo_router.put("/todo/{todo_id}")
 async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
